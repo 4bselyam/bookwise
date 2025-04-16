976bab55cb54978395f5851a60d06f9cc7835b83
Akhmed Valiaiev <valiaiev@faber-solartechnik.de>
2025-04-16 15:44:46 +0200

    Finish designing auth and home page

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren } from 'react'
+import Header from '@/components/Header'
+const Layout = ({ children }: PropsWithChildren) => {
+	return (
+		<main className='root-container'>
+			<div className='mx-auto max-w-7xl'>
+				<Header />
+				<div className='mt-20 pb-20'>{children}</div>
+			</div>
+		</main>
+	)
+}
+
+export default Layout
